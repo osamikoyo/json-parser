78e6092b4475f75574812e6065f4a1466e3e4adf
osamikoyo <v.pylayev@mail.ru>
2026-02-27 06:49:51 +0500

    update lexer.h

--- src/lexer.h
+++ src/lexer.h
@@ -1,22 +1,26 @@
+#include "tokenizer.h"
+
 typedef struct Object Object;
 typedef struct Pair Pair;
 
-enum ValueType{
-    OBJECT,
-    VALUE,
+enum ValueType {
+  OBJECT,
+  VALUE,
 };
 
-typedef union{
-    char *value;
-    Object *obj;
+typedef union {
+  char *value;
+  Object *obj;
 } Value;
 
-struct Object{
-    Pair *pairs;
+struct Object {
+  Pair *pairs;
 };
 
-struct Pair{
-    char *key;
-    ValueType type;
-    Value value;
+struct Pair {
+  char *key;
+  enum ValueType type;
+  Value value;
 };
+
+Object *tokens_to_ast(Token *tokens);
